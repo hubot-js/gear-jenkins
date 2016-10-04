8a85c6bf0315cdae1bee4e733d67dbdd69673383
robsonbittencourt <robson.luizv@gmail.com>
2016-10-03 22:31:24 -0300

    Refactory to use talk instead postMessage

--- src/handlers/start-job.js
+++ src/handlers/start-job.js
@@ -9,16 +9,14 @@ function handle(hubot, message, task, params) {
 }
 
 function start(hubot, message, task, job) {
-   var recipient = hubot.getRecipient(message);
-
    return jenkins.callJob(job).then(function() {
-      hubot.postMessage(recipient, task.options.message, {as_user: true});  
+      hubot.talk(message, task.options.message);  
    }, function(error) {
       if (error.notFound) {
-         hubot.postMessage(recipient, `Sorry I could not find the job *${job}*`, {as_user: true});
+         hubot.talk(message, `Sorry I could not find the job *${job}*`);
       } else {
          hubot.detailedError('Error on call Jenkins', error);
-         hubot.postMessage(recipient, `Sorry I could not start the job *${job}*. See the error in the logs.`, {as_user: true});
+         hubot.talk(message, `Sorry I could not start the job *${job}*. See the error in the logs.`);
       }
    });
 };    
